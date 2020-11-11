05fe1815aa27c5df9ae5c110c28f653165f9dcdc
KhinMoeHtet <khinmoehtetucsh@gmail.com>
2020-11-11 10:28:30 +0630

    resolve conflict

--- app/src/main/java/com/kmh/moviedb/MainActivity.kt
+++ app/src/main/java/com/kmh/moviedb/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kmh.moviedb
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.SearchView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +14,13 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kmh.moviedb.model.NowPlayingModel
+import com.kmh.moviedb.model.ResultsItem
+import com.kmh.moviedb.ui.nowPlaying.MovieAdapter
+import com.kmh.moviedb.ui.nowPlaying.NowPlayingViewModel
+import kotlinx.android.synthetic.main.fragment_now_playing.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,12 +48,28 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
+        val menuItem = menu?.findItem(R.id.action_settings)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.queryHint = "Type Movie Name"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean {
+                var searchQuery = "%$newText%"
+               // NowPlayingViewModel.getNowPlayingArticle(searchQuery)
+               //         .observe(this@MainActivity, Observer { MovieAdapter.getItemCount(it as List<ResultsItem>) })
+                //nowPlayingRecycler.layoutManager = LinearLayoutManager(applicationContext)
+              //  nowPlayingRecycler.adapter = MovieAdapter
+                return true
+            }
+
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
